3a97db6cf1588edc6673286fae98ba923b5cf6f8
7slavec <7slavec@gmail.com>
2026-09-15 17:28:56 +0400

    Initial Commit

---
+++ Memory/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Memory
+//
+//  Created by Вячеслав Храмышкин on 15.09.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MemoryTests/MemoryTests.swift
@@ -0,0 +1,18 @@
+//
+//  MemoryTests.swift
+//  MemoryTests
+//
+//  Created by Вячеслав Храмышкин on 15.09.2026.
+//
+
+import Testing
+
+struct MemoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
